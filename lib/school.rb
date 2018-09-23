390edf25927d3421d73f115318a3f6bc37664d5c
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-23 09:30:57 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,18 @@
 # code here!
 class School
-  attr_accessor :roster
+  attr_reader :roster
 
   def initialize(name)
     @name = name
     @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade] 
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
   end
 end
