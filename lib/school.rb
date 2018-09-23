12cd1e4a31c06f84a31934d413f813657271090c
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-23 09:42:50 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,15 +8,15 @@ class School
   end
 
   def add_student(student_name, grade)
-    if @roster[grade]
-      @roster[grade] << student_name
-    else
-      @roster[grade] = []
-      @roster[grade] << student_name
-    end
+    @roster[grade] ||= []
+    @roster[grade] << student_name
   end
 
   def grade(grade)
     @roster[grade]
+  end
+
+  def sort
+    @roster.map {|key, value| key => value.sort}
   end
 end
